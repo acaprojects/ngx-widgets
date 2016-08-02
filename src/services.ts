b93d7e32579919e8ade404b0d44e77091f897fba
Alex Sorafumo <alex@yuion.net>
2016-08-02 12:49:01 +1000

    Fix to services. Update readme.

--- src/services.ts
+++ src/services.ts
@@ -1,10 +1,15 @@
 // Import all services
 import { ModalService } from './components/modal';
 import { NotificationService } from './components/notification';
+import { ACA_Animate } from './services';
+
 // Export all services
+export * from './components/modal';
+export * from './components/notification';
 
 // Export convenience property
 export const ACA_WIDGET_PROVIDERS: any[] = [
 	ModalService, 
-	NotificationService
+	NotificationService,
+	ACA_Animate
 ];
