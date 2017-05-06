7aef6579ce5627f9c740d8f1de756334c2ca5906
Alex Sorafumo <alex@yuion.net>
2017-05-06 11:58:44 +1000

    Fix more linter errors

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 /**
-* @Author: Alex Sorafumo
-* @Date:   04/10/2016 12:25 PM
-* @Email:  alex@yuion.net
-* @Filename: index.ts
-* @Last modified by:   alex.sorafumo
-* @Last modified time: 24/01/2017 4:58 PM
-*/
+ * @Author: Alex Sorafumo
+ * @Date:   04/10/2016 12:25 PM
+ * @Email:  alex@yuion.net
+ * @Filename: index.ts
+ * @Last modified by:   alex.sorafumo
+ * @Last modified time: 24/01/2017 4:58 PM
+ */
 
 export * from './_build';
