8777e7287225aff11e03d1383d3ee0274e2f696c
Alex Sorafumo <alex@yuion.net>
2017-11-15 11:20:49 +1100

    bump version

--- src/index.ts
+++ src/index.ts
@@ -68,8 +68,8 @@ export class MyHammerConfig extends HammerGestureConfig {
 export class WidgetsModule {
     private static init: boolean = false;
 
-    private version: string = '0.13.11';
-    private build: string = '2017-11-14.v1';
+    private version: string = '0.13.12';
+    private build: string = '2017-11-15.v1';
 
     constructor() {
         if (!WidgetsModule.init) {
