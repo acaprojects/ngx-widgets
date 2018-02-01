bb0d1d6a5eaf4c56e005feb45058ef9434c2fee2
Alex Sorafumo <alex@yuion.net>
2018-02-01 11:19:00 +1100

    [docs] Add binding display to showcase component

--- src/app/shared/components/showcase/showcase.component.ts
+++ src/app/shared/components/showcase/showcase.component.ts
@@ -10,8 +10,8 @@ import * as hljs from 'highlight.js';
     styleUrls: ['./showcase.styles.scss'],
     animations: [
         trigger('show', [
-            transition(':enter', [style({ opacity: 0 }), animate('300ms ease-in', style({ opacity: 1 }))]),
-            transition(':leave', [style({ opacity: 1 }), animate('300ms ease-out', style({ opacity: 0 }))])
+            transition(':enter', [style({ opacity: 0, position: 'absolute', left: 0, right: 0 }), animate('300ms ease-in', style({ opacity: 1 }))]),
+            transition(':leave', [style({ opacity: 1, position: 'absolute', left: 0, right: 0 }), animate('300ms ease-out', style({ opacity: 0 }))])
         ])
     ]
 })
@@ -24,6 +24,7 @@ export class ShowcaseComponent {
     @ViewChildren('tab') private tab_list: QueryList<ElementRef>;
 
     @ViewChild('inject') private inject_block: ElementRef;
+    @ViewChild('binding') private binding_block: ElementRef;
 
     public ngOnInit() {
         this.state.category_list = ['overview', 'bindings', 'playground'];
@@ -32,6 +33,7 @@ export class ShowcaseComponent {
 
     public setCategory(value: string) {
         this.state.category = value;
+        this.state.binding = null;
         const tabs = this.tab_list.toArray();
         const tab = tabs[this.state.category_list.indexOf(value)];
         if (tab) {
@@ -45,10 +47,19 @@ export class ShowcaseComponent {
         setTimeout(() => this.highlight(), 50);
     }
 
+    public selectBinding(binding: any) {
+        this.state.binding = binding;
+        setTimeout(() => this.highlight(), 50);
+    }
+
     public highlight() {
         if (this.inject_block && this.inject_block.nativeElement) {
             console.log('Highlight element');
             hljs.highlightBlock(this.inject_block.nativeElement);
+        }
+        if (this.binding_block && this.binding_block.nativeElement) {
+            console.log('Highlight element');
+            hljs.highlightBlock(this.binding_block.nativeElement);
         }
     }
 }
--- src/app/shell/general-widgets/map-showcase/map-showcase.component.ts
+++ src/app/shell/general-widgets/map-showcase/map-showcase.component.ts
@@ -12,7 +12,13 @@ export class MapShowcaseComponent {
     public model: any = {
         title: 'Interactive Map',
         bindings: [
-            { name: 'src', type: 'input', description: 'Path to the map file. Only supports SVG files' }
+            { name: 'src', type: 'input', description: 'Path to the map file. Only supports SVG files', data: 'string' },
+            { name: 'styles', type: 'input', description: 'Map of styles to apply to the displayed SVG map', data: 'object' },
+            { name: 'poi', type: 'input', description: 'Points of interest which components can be placed', data: 'object[]' },
+            { name: 'focus', type: 'input', description: 'Element within the map to focus upon', data: 'object[]' },
+            { name: 'zoom', type: 'both', description: 'Zoom level of the map 0 is 100%', data: 'number' },
+            { name: 'center', type: 'both', description: 'Element within the map to focus upon', data: 'object' },
+            { name: 'event', type: 'output', description: 'Events posted by the map get emitted', data: 'object' }
         ],
         inject: '',
         map: {}
@@ -21,6 +27,7 @@ export class MapShowcaseComponent {
     public ngOnInit() {
         this.model.inject = `&lt;map [src]=&quot;map_file&quot;
      [styles]=&quot;map_styles&quot;
+     [poi]=&quot;map_styles&quot;
      [focus]=&quot;map_focus&quot;
      (event)=&quot;doSomething($event)&quot;&gt;
 &lt;/map&gt;`;
