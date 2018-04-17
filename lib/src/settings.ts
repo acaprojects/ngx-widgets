dca4cb2ad4a7b22d5901de8d19ccac22e0c2a0e8
Alex Sorafumo <alex@yuion.net>
2018-04-17 11:13:38 +1000

    Tweak user input event for buttons

--- lib/src/settings.ts
+++ lib/src/settings.ts
@@ -15,7 +15,7 @@ export class WIDGETS {
     private static _obs: any = {};
     private static timer: any = null;
     private static load_count = 0;
-    public static app_version = '0.14.26';
+    public static app_version = '0.14.27';
 
     public static init() {
         setTimeout(() => {
