73e174c076affdfa27c27cc414551e0b89537f48
Alex Sorafumo <alex@yuion.net>
2018-05-07 09:44:34 +1000

    Bump version

--- lib/src/settings.ts
+++ lib/src/settings.ts
@@ -15,7 +15,7 @@ export class WIDGETS {
     private static _obs: any = {};
     private static timer: any = null;
     private static load_count = 0;
-    public static app_version = '0.14.32';
+    public static app_version = '0.14.33';
 
     public static init() {
         setTimeout(() => {
