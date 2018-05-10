0fe6958990dcb48e6918e47d80bf9b61af85f845
Alex Sorafumo <alex@yuion.net>
2018-05-10 13:02:46 +1000

    Fix pinch handler on map input directive to correctly scale the map

--- lib/src/widgets.module.ts
+++ lib/src/widgets.module.ts
@@ -239,7 +239,7 @@ export class WidgetsHammerConfig extends HammerGestureConfig {
 })
 export class WidgetsModule {
     private static init = false;
-    private build = '2018-05-08.v1';
+    private build = '2018-05-10.v1';
 
     constructor() {
         if (!WidgetsModule.init) {
