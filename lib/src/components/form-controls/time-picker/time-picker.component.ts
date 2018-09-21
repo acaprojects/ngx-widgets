dff61700cc1ba4f1148d1235937973ec35c9c8df
Alex Sorafumo <alex@yuion.net>
2018-09-21 12:38:52 +1000

    Update time picker component

--- lib/src/components/form-controls/time-picker/time-picker.component.ts
+++ lib/src/components/form-controls/time-picker/time-picker.component.ts
@@ -29,10 +29,12 @@ export class TimePickerComponent implements OnInit, OnChanges {
     @Input() date: string;
     @Input() duration: number;
     @Input() range = false;
+    @Input() manual = true;
     @Output() dateChange = new EventEmitter();
     @Output() durationChange = new EventEmitter();
 
     public model: any = {};
+    public timers: any = {};
     public display: any = {};
 
     public ngOnInit() {
@@ -82,23 +84,25 @@ export class TimePickerComponent implements OnInit, OnChanges {
     }
 
     public set(item: any) {
+        if (this.model.transition) { return; }
         setTimeout(() => {
             this.model.selection = item.index;
             switch(this.model.active.period) {
                 case TimePickerPeriod.START_HOUR:
                     this.model.date.hours((+item.value % 12) + (this.model.active.afternoon ? 12 : 0));
                     this.updateEnd();
-                    this.select(1);
+                    this.model.transition = true;
+                    setTimeout(() => this.select(1), 800);
                     break;
                 case TimePickerPeriod.START_MINUTE:
                     this.model.date.minutes(+item.value);
                     this.updateEnd();
-                    if (this.range) { this.select(2); }
                     break;
                 case TimePickerPeriod.END_HOUR:
                     this.model.end.hours((+item.value % 12) + (this.model.active.afternoon ? 12 : 0));
                     this.updateDuration();
-                    this.select(3);
+                    this.model.transition = true;
+                    setTimeout(() => this.select(3), 800);
                     break;
                 case TimePickerPeriod.END_MINUTE:
                     this.model.end.minutes(+item.value);
@@ -106,10 +110,11 @@ export class TimePickerComponent implements OnInit, OnChanges {
                     break;
             }
             this.updateDisplay();
-        }, 50);
+        }, 10);
     }
 
     public select(period: TimePickerPeriod) {
+        this.model.transition = false;
         this.model.active = {
             period,
             afternoon: false
@@ -178,10 +183,66 @@ export class TimePickerComponent implements OnInit, OnChanges {
         }
     }
 
+    public checkFields(end: boolean = true) {
+        if (this.timers.check) {
+            clearTimeout(this.timers.check);
+        }
+        this.timers.check = setTimeout(() => {
+                // Check start hour
+            if (this.model.start_hour) {
+                console.log('Start hour:', +this.model.start_hour < 0, +this.model.start_hour >= 24, isNaN(this.model.start_hour))
+                this.model.error_sh = +this.model.start_hour < 0 || +this.model.start_hour >= 24 || isNaN(this.model.start_hour);
+                if (!this.model.error_sh) {
+                    this.model.date.hour(+this.model.start_hour);
+                }
+            }
+                // Check start minute
+            if (this.model.start_minute) {
+                this.model.error_sm = +this.model.start_minute < 0 || +this.model.start_minute >= 60 || isNaN(this.model.start_minute);
+                if (!this.model.error_sm) {
+                    this.model.date.minute(+this.model.start_minute);
+                }
+            }
+                // Check end hour
+            if (this.model.end_hour) {
+                this.model.error_eh = +this.model.end_hour < 0 || +this.model.end_hour >= 24 || isNaN(this.model.end_hour);
+                if (!this.model.error_eh) {
+                    this.model.end.hour(+this.model.end_hour);
+                }
+            }
+                // Check end minute
+            if (this.model.end_minute) {
+                this.model.error_em = +this.model.end_minute < 0 || +this.model.end_minute >= 60 || isNaN(this.model.end_minute);
+                if (!this.model.error_em) {
+                    this.model.end.minute(+this.model.end_minute);
+                }
+            }
+            if (end) {
+                this.updateDuration()
+            } else {
+                this.updateEnd();
+            }
+            this.updateDisplay();
+            this.timers.check = null;
+        }, 300)
+    }
+
+    public toggleState() {
+        this.model.manual = !this.model.manual;
+        this.model.start_hour = this.model.date.hours();
+        this.model.start_minute = this.model.date.minutes();
+        this.model.end_hour = this.model.end.hours();
+        this.model.end_minute = this.model.end.minutes();
+        this.select(0);
+    }
+
+
     private updateEnd() {
         if (this.model.date && this.range) {
             this.model.end = moment(this.model.date);
-            this.model.end.add(this.duration || 60, 'm');
+            this.model.end.add(this.model.duration || 60, 'm');
+            this.model.end_hour = this.model.end.hours();
+            this.model.end_minute = this.model.end.minutes();
         }
         if (this.model.date) {
             this.dateChange.emit(this.model.date.format('HH:mm'));
