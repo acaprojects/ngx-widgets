2352336a8d8ebf65812566d5e7571ce202f4ffe3
Alex Sorafumo <alex@yuion.net>
2018-10-24 09:53:23 +1100

    Tweak context item to be openable on touch/click

--- lib/src/directives/context-item.directive.ts
+++ lib/src/directives/context-item.directive.ts
@@ -11,6 +11,7 @@ export class ContextItemDirective {
     @Input() public id: string = `CTX_${Math.floor(Math.random() * 8999999 + 1000000)}`;
     @Input() public cmp: Type;
     @Input() public data: any;
+    @Input() public touch: boolean = false;
     @Input() public template: TemplateRef<any>;
     @Output() public offset = new EventEmitter();
     @Output() public event = new EventEmitter();
@@ -18,27 +19,35 @@ export class ContextItemDirective {
     private model: any = {};
 
     @HostListener('contextmenu', ['$event']) public contextEvent(event) {
+        this.open(event)
+    }
+
+    @HostListener('touchrelease', ['$event']) public touchEvent(event) {
+        if (this.touch) { this.open(event) }
+    }
+
+    public open(event) {
         event.preventDefault();
         if (this.model.instance) {
             this.overlay.remove('root', this.model.instance.id);
             this.model.instance = null;
-            return setTimeout(() => this.contextEvent(event), 50);
+            return setTimeout(() => this.open(event), 50);
         }
         const h = window.innerHeight;
         const w = window.innerWidth;
-        const offset = { 
-            top: event.clientY, 
-            left: event.clientX, 
-            right:  w - event.clientX, 
+        const offset = {
+            top: event.clientY,
+            left: event.clientX,
+            right:  w - event.clientX,
             bottom: h - event.clientY
         };
-        const data = { 
+        const data = {
             offset,
             data: this.data
         };
-        this.overlay.add('root', `context_${this.id}`, ContextItemComponent, { 
-            cmp: this.cmp, 
-            template: this.template, 
+        this.overlay.add('root', `context_${this.id}`, ContextItemComponent, {
+            cmp: this.cmp,
+            template: this.template,
             data
         }, (e) => {
             this.event.emit(e);
