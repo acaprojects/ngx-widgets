0f7a9acf9a060604b3c4b9742a797efcaead60c7
Alex Sorafumo <alex@yuion.net>
2019-03-08 11:47:56 +1100

    Bump version

--- lib/src/widgets.module.ts
+++ lib/src/widgets.module.ts
@@ -64,8 +64,8 @@ export class WidgetsHammerConfig extends HammerGestureConfig {
 })
 export class WidgetsModule {
     private static init = false;
-    private build = moment(1551913740000);
-    public static version = '0.26.13';
+    private build = moment(1552005960000);
+    public static version = '0.26.14';
 
     constructor() {
         if (!WidgetsModule.init) {
