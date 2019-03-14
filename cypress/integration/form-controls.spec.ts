d05cbb16e8a47a2d3bc1783eacb82f6e9a577899
Alex Sorafumo <alex@yuion.net>
2019-03-14 11:03:52 +1100

    Add cypress tests for time picker component

--- cypress/integration/form-controls.spec.ts
+++ cypress/integration/form-controls.spec.ts
@@ -39,3 +39,63 @@ context('Calendar', () => {
         cy.contains(`calendar-showcase .container > .date`, next_month.format('DD MMM YYYY')).should('be.visible');
     })
 });
+
+context('Time Picker', () => {
+    const widget = `time-picker-showcase`;
+
+    it('Load form controls', () => {
+        cy.visit('http://localhost:4200/#/form-controls/time-picker');
+        cy.wait(1000);
+    });
+
+    it('Time picker should be visible', () => {
+        cy.get(`${widget} [widget]`).should('be.visible');
+    });
+
+    it('Change start time to 11:30', () => {
+        cy.contains(`${widget} [widget] .clockface .value`, '11').click();
+        cy.wait(200);
+        cy.contains(`${widget} [widget] .group-item .start .hours`, '11').should('be.visible');
+        cy.wait(1000);
+        cy.contains(`${widget} [widget] .clockface`, '00').should('be.visible');
+        cy.contains(`${widget} [widget] .clockface .value`, '30').click();
+        cy.wait(200);
+        cy.contains(`${widget} [widget] .group-item .start .minutes`, '30').scrollIntoView().should('be.exist');
+    });
+
+    it('Change end time to 1:30', () => {
+        cy.get(`${widget} [widget] .group-item .end .hours`).click()
+        cy.contains(`${widget} [widget] .clockface .value`, ' 1 ').click();
+        cy.wait(200);
+        cy.contains(`${widget} [widget] .group-item .end .hours`, '01').should('be.visible');
+        cy.wait(1000);
+        cy.contains(`${widget} [widget] .clockface`, '00').should('be.visible');
+        cy.contains(`${widget} [widget] .clockface .value`, '30').click();
+        cy.wait(200);
+        cy.contains(`${widget} [widget] .group-item .end .minutes`, '30').scrollIntoView().should('be.exist');
+        cy.contains(`${widget} [widget] .duration`, ' 2 hours').should('be.visible');
+    });
+
+    it('Check start period change', () => {
+            //
+        if (cy.contains(`${widget} [widget] .group-item .start .period.active`, 'AM')) {
+            cy.get(`${widget} [widget] .group-item .start .block.period`).click();
+            cy.contains(`${widget} [widget] .group-item .start .period.active`, 'PM').should('be.visible');
+        } else {
+            cy.get(`${widget} [widget] .group-item .start .block.period`).click();
+            cy.contains(`${widget} [widget] .group-item .start .period.active`, 'AM').should('be.visible');
+        }
+        cy.contains(`${widget} [widget] .duration`, '2 hours').should('be.visible');
+    });
+
+    it('Check end period change', () => {
+        if (cy.contains(`${widget} [widget] .group-item .end .period.active`, 'AM')) {
+            cy.get(`${widget} [widget] .group-item .end .block.period`).click();
+            cy.contains(`${widget} [widget] .group-item .end .period.active`, 'PM').should('be.visible');
+        } else {
+            cy.get(`${widget} [widget] .group-item .end .block.period`).click();
+            cy.contains(`${widget} [widget] .group-item .end .period.active`, 'AM').should('be.visible');
+        }
+        cy.contains(`${widget} [widget] .duration`, ' 14 hours').should('be.visible');
+    })
+});
