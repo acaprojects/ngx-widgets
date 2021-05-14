0f0387c31e1cd2c9bda4b5b3e2ccba8bba8100bd
Alex Sorafumo <alex@yuion.net>
2021-05-14 13:02:47 +1000

    fix(dynamic-form): fix emitting form changes and whether form is valid

--- lib/src/components/form-controls/dynamic-form/dynamic-form.component.ts
+++ lib/src/components/form-controls/dynamic-form/dynamic-form.component.ts
@@ -1,22 +1,33 @@
-
-import { Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output, SimpleChanges } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { BaseWidgetComponent } from '../../../shared/base.component';
-import { DynamicField } from './dynamic-field.class';
+import {
+    Component,
+    EventEmitter,
+    Input,
+    OnChanges,
+    OnDestroy,
+    OnInit,
+    Output,
+    SimpleChanges,
+} from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { BaseWidgetComponent } from "../../../shared/base.component";
+import { DynamicField } from "./dynamic-field.class";
 
 @Component({
-    selector: 'dynamic-form',
-    templateUrl: './dynamic-form.template.html',
-    styleUrls: ['./dynamic-form.styles.scss']
+    selector: "dynamic-form",
+    templateUrl: "./dynamic-form.template.html",
+    styleUrls: ["./dynamic-form.styles.scss"],
 })
-export class DynamicFormComponent extends BaseWidgetComponent implements OnChanges, OnInit, OnDestroy {
+export class DynamicFormComponent
+    extends BaseWidgetComponent
+    implements OnChanges, OnInit, OnDestroy
+{
     @Input() public fields: DynamicField<any>[] = [];
     @Input() public validate: boolean;
     @Output() public form = new EventEmitter();
     @Output() public valid = new EventEmitter();
 
     public form_group: FormGroup;
-    public no_emit: boolean;
+    public last_submit = "";
 
     public ngOnInit() {
         this.form_group = this.makeFormGroup(this.fields);
@@ -28,7 +39,7 @@ export class DynamicFormComponent extends BaseWidgetComponent implements OnChang
             this.form_group.valueChanges.subscribe(() => this.emitForm());
         }
         if (changes.validate) {
-            this.form_group?.markAsTouched();
+            this.form_group ? this.form_group.markAsTouched() : "";
             this.emitForm();
         }
     }
@@ -36,26 +47,35 @@ export class DynamicFormComponent extends BaseWidgetComponent implements OnChang
     public makeFormGroup(fields: DynamicField<any>[]) {
         const group: any = {};
 
-        const handleItem = item => {
+        const handleItem = (item) => {
             let validators = [];
-            if (item.required) { validators.push(Validators.required); }
+            if (item.required) {
+                validators.push(Validators.required);
+            }
             if (item.validators && item.validators.length > 0) {
                 validators = [...validators, ...item.validators];
             }
             if (item.children) {
                 item.children.forEach(handleItem);
             }
-            group[item.key] = validators && validators.length > 0 ?
-                new FormControl({ value: item.value, disabled: item.disabled }, validators) :
-                new FormControl({ value: item.value, disabled: item.disabled });
-            if (item.dirty) { (group[item.key] as FormControl).markAsDirty(); }
+            group[item.key] =
+                validators && validators.length > 0
+                    ? new FormControl(
+                          { value: item.value, disabled: item.disabled },
+                          validators
+                      )
+                    : new FormControl({
+                          value: item.value,
+                          disabled: item.disabled,
+                      });
+            if (item.dirty) {
+                (group[item.key] as FormControl).markAsDirty();
+            }
         };
 
         if (fields && fields instanceof Array) {
             fields.forEach(handleItem);
         }
-        this.no_emit = true;
-        this.timeout('init', () => this.no_emit = false, 1000);
         return new FormGroup(group);
     }
 
@@ -65,7 +85,16 @@ export class DynamicFormComponent extends BaseWidgetComponent implements OnChang
     }
 
     public emitForm() {
-        if (this.no_emit || !this.form_group?.valid) return;
+        const str = JSON.stringify(this.form_group.value);
+        console.log("String:", str === this.last_submit);
+        if (
+            !this.form_group ||
+            !this.form_group.valid ||
+            this.last_submit === str
+        )
+            return;
+        this.last_submit = str;
         this.form.emit(this.form_group.value);
+        this.valid.emit(this.form_group.valid);
     }
 }
